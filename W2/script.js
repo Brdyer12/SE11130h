5ad8640f1e76bcfee72cff56d121c29e6ee50663
Brdyer12 <brdyer@email.neit.edu>
2025-03-31 10:37:19 -0400

    end of class

---
+++ W2/script.js
@@ -0,0 +1,7 @@
+
+    var message = prompt("What is your name");
+alert(message + ", Wouldnt you like to be a pepper too?");
+document.getElementById("welcome-message").innerHTML = `Welcome ${message} to week 2 lab.`;
+document.getElementById("footer").style.color = "red";
+
+ 
